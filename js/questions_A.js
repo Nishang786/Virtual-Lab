b317de254b1dd6f762a8a698abfc470c7d6c38ac
Nishang786 <nishang786@gmail.com>
2022-05-22 09:35:04 +0530

    new

---
+++ js/questions_A.js
@@ -0,0 +1,29 @@
+// creating an array and passing the number, questions, options, and answers
+let questions_A = [
+    {
+    numb: 1,
+    marks: 3,
+    question: "What does HTML stand for?",
+    answer:1,
+    options: [
+      "C:/Users/hp/OneDrive/Desktop/AJ_Projects/Quiz Application with Timer/Screenshot 2022-04-30 180602.png",
+      "C:/Users/hp/OneDrive/Desktop/AJ_Projects/Quiz Application with Timer/Screenshot 2022-04-30 180545.png",
+      "C:/Users/hp/OneDrive/Desktop/AJ_Projects/Quiz Application with Timer/Screenshot 2022-04-30 180521.png",
+      "C:/Users/hp/OneDrive/Desktop/AJ_Projects/Quiz Application with Timer/Screenshot 2022-04-30 180501.png"
+    ]
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/questions_B.js
@@ -0,0 +1,21 @@
+let questions_B = [
+    {
+        numb : 1,
+        marks: 3,
+        question: "Shaft",
+        answer : '2'
+
+    },
+    {
+        numb : 2,
+        marks : 3,
+        question:'Sunk Key',
+        answer : '2'
+    },
+    {
+        numb : 3,
+        marks : 3,
+        question:'Sleeve',
+        answer : '1'
+    }
+]
